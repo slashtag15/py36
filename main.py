b8d70d7cf4248f6300c306359d181892e3570470
slashtag15 <naman.m2_mhs@gemselearning.com>
2026-01-31 13:19:10 +0400

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pyg
+import random
 pyg.init()
 W=800
 H=600
@@ -8,19 +9,29 @@ bg=pyg.image.load("back.jpg")
 roc=pyg.image.load("rock.png")
 fir=pyg.image.load("magic.png")
 bg=pyg.transform.scale(bg,(W,H))
-roc=pyg.transform.scale(roc,(60,120))
-fir=pyg.transform.scale(fir,(50,70))
+roc=pyg.transform.scale(roc,(60,100))
+fir=pyg.transform.scale(fir,(80,120))
 
 don=False
 FPS=60
+x=350
+xx=400
 clc=pyg.time.Clock()
 while not don:
     clc.tick(FPS)
     for ev in pyg.event.get():
         if ev.type==pyg.QUIT:
             pyg.quit()
-
+        key=pyg.key.get_pressed()
+    if key[pyg.K_RIGHT]:
+        x=x+5
+    if key[pyg.K_LEFT]:
+        x=x-5
+    if x>740:
+        x=350
+    if x<10:
+        x=350
     scr.blit(bg,(0,0))
-    scr.blit(roc,(350,500))
-    scr.blit(fir,(400,50))
+    scr.blit(roc,(x,500))
+    scr.blit(fir,(xx,50))
     pyg.display.flip()
